694672882b01dcc757d493802729ed83d871eab2
ashokkushh <ashokkushwaha7983@gmail.com>
2025-10-31 19:49:21 +0530

    1

--- Linked_List/LinkedList.cpp
+++ Linked_List/LinkedList.cpp
@@ -1,5 +1,20 @@
 #include<iostream>
 
+class Node{
+ 	int data;
+	node* next;
+
+public:
+	node(int val){
+		data = val;
+		next = NULL;
+	}
+
+};
+
+class List{
+	
+};
 int main(){
 
 std::cout<<"Hello I am LL"<<std::endl;
