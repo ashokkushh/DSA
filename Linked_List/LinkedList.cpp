33b50bf8712bc37242ce5d60fbb239b3a3d02df7
ashokkushh <ashokkushwaha7983@gmail.com>
2025-11-09 22:38:03 +0530

    pushf & pushb added

--- Linked_List/LinkedList.cpp
+++ Linked_List/LinkedList.cpp
@@ -1,24 +1,69 @@
 #include<iostream>
+using namespace std;
 
 class Node{
- 	int data;
-	node* next;
-
 public:
-	node(int val){
-		data = val;
+	int val;
+	Node* next;
+
+	Node(int data){
+		val = data;
 		next = NULL;
 	}
 
 };
 
 class List{
+	Node* head;
+public:
+	List(){
+		head = NULL;
+	}
+	void pushf(int data){
+		if(head == NULL){
+			head = new Node(data);
+		}else{
+			Node* newnode = new Node(data);
+			newnode->next = head;
+			head = newnode;
+		}
+	}
+	
+	void pushb(int data){
+		if(head == NULL){
+			head = new Node(data);
+		}else{
+			Node* temp = head;
+			while(temp->next != NULL){
+				temp = temp->next;
+			}
+			Node* newnode = new Node(data);
+			temp->next = newnode;
+		}
+	}
+	void print(){
+		Node* temp = head;
+		while(temp != NULL){
+			cout<<temp->val<<"-->";
+			temp = temp->next;
+		}
+		cout<<endl;
+	
+	}
+	
 	
 };
+
 int main(){
+	cout<<"Hlo! Its main Function"<<endl;
+	List l;
+	l.pushf(1);
+	l.print();
+	l.pushf(2);
+	l.print();
+	l.pushf(3);
+	l.pushb(0);
+	l.pushb(-1);
 
-std::cout<<"Hello I am LL"<<std::endl;
-
-return 0;
-
+	l.print();
 }
